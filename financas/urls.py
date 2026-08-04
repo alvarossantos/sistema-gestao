a3f5d6429f722a7a8973036d5a1f6d50543df2f3
alvarossantos <alvaroguimaraessantos@gmail.com>
2026-08-04 19:02:23 -0300

    feat: adicionar templates para categorias, centros de custo e cartões de crédito
    - Criar templates CRUD para categorias (list, create, detail, update)
    - Criar templates CRUD para centros de custo (list, create, detail, update)
    - Criar templates CRUD para cartões de crédito (list, create, detail, update)
    - Adicionar links de navegação no navbar (Categorias, Centros de Custo, Cartões)
    - Corrigir success_url das views de Categorias
    - Corrigir success_url das views de CentroCusto

--- financas/urls.py
+++ financas/urls.py
@@ -9,5 +9,19 @@ urlpatterns = [
     path("listarcontas/nova/", views.FinancasContaCreateView.as_view(), name="nova"),
     path("listarcontas/<int:pk>/", views.FinancasContaDetailView.as_view(), name="detalhe"),
     path("listarcontas/<int:pk>/editar/", views.FinancasContaUpdateView.as_view(), name="editar"),
-    
+
+    path("listarcategorias/", views.FinancasCategoriaListView.as_view(), name="listarcategorias"),
+    path("listarcategorias/nova/", views.FinancasCategoriaCreateView.as_view(), name="novacategoria"),
+    path("listarcategorias/<int:pk>/", views.FinancasCategoriaDetailView.as_view(), name="detalhecategoria"),
+    path("listarcategorias/<int:pk>/editar/", views.FinancasCategoriaUpdateView.as_view(), name="editarcategoria"),
+
+    path("listarcentrocustos/", views.FinancasCentroCustoListView.as_view(), name="listarcentrocustos"),
+    path("listarcentrocustos/nova/", views.FinancasCentroCustoCreateView.as_view(), name="novacentrocusto"),
+    path("listarcentrocustos/<int:pk>/", views.FinancasCentroCustoDetailView.as_view(), name="detalhecentrocusto"),
+    path("listarcentrocustos/<int:pk>/editar/", views.FinancasCentroCustoUpdateView.as_view(), name="editarcentrocusto"),
+
+    path("cartao/", views.FinancasCartaoCreditoListView.as_view(), name="listarcartao"),
+    path("cartao/nova/", views.FinancasCartaoCreditoCreateView.as_view(), name="novacartao"),
+    path("cartao/<int:pk>/", views.FinancasCartaoCreditoDetailView.as_view(), name="detalhecartao"),
+    path("cartao/<int:pk>/editar/", views.FinancasCartaoCreditoUpdateView.as_view(), name="editarcartao"),
 ]
--- financas/views.py
+++ financas/views.py
@@ -42,3 +42,132 @@ class FinancasContaDetailView(LoginRequiredMixin, DetailView):
 
     def get_queryset(self):
         return super().get_queryset().filter(usuario=self.request.user)
+
+
+class FinancasCategoriaListView(LoginRequiredMixin, ListView):
+    model = models.Categoria
+    template_name = "categorias_list.html"
+    context_object_name = "categorias"
+
+    def get_queryset(self):
+        return super().get_queryset().filter(usuario=self.request.user)
+
+
+class FinancasCategoriaCreateView(LoginRequiredMixin, CreateView):
+    model = models.Categoria
+    template_name = "categorias_create.html"
+    form_class = forms.CategoriaForm
+    success_url = reverse_lazy("financas:listarcategorias")
+
+    def form_valid(self, form):
+        form.instance.usuario = self.request.user
+        return super().form_valid(form)
+
+
+class FinancasCategoriaUpdateView(LoginRequiredMixin, UpdateView):
+    model = models.Categoria
+    template_name = "categorias_update.html"
+    form_class = forms.CategoriaForm
+    success_url = reverse_lazy("financas:listarcategorias")
+
+    def get_queryset(self):
+        return super().get_queryset().filter(usuario=self.request.user)
+
+    def form_valid(self, form):
+        form.instance.usuario = self.request.user
+        return super().form_valid(form)
+
+
+class FinancasCategoriaDetailView(LoginRequiredMixin, DetailView):
+    model = models.Categoria
+    template_name = "categorias_detail.html"
+    context_object_name = "categoria"
+
+    def get_queryset(self):
+        return super().get_queryset().filter(usuario=self.request.user)
+
+
+class FinancasCentroCustoListView(LoginRequiredMixin, ListView):
+    model = models.CentroCusto
+    template_name = "centrocustos_list.html"
+    context_object_name = "centrocustos"
+
+    def get_queryset(self):
+        return super().get_queryset().filter(usuario=self.request.user)
+
+
+class FinancasCentroCustoCreateView(LoginRequiredMixin, CreateView):
+    model = models.CentroCusto
+    template_name = "centrocustos_create.html"
+    form_class = forms.CentroCustoForm
+    success_url = reverse_lazy("financas:listarcentrocustos")
+
+    def form_valid(self, form):
+        form.instance.usuario = self.request.user
+        return super().form_valid(form)
+
+
+class FinancasCentroCustoUpdateView(LoginRequiredMixin, UpdateView):
+    model = models.CentroCusto
+    template_name = "centrocustos_update.html"
+    form_class = forms.CentroCustoForm
+    success_url = reverse_lazy("financas:listarcentrocustos")
+
+    def get_queryset(self):
+        return super().get_queryset().filter(usuario=self.request.user)
+
+    def form_valid(self, form):
+        form.instance.usuario = self.request.user
+        return super().form_valid(form)
+
+
+class FinancasCentroCustoDetailView(LoginRequiredMixin, DetailView):
+    model = models.CentroCusto
+    template_name = "centrocustos_detail.html"
+    context_object_name = "centrocusto"
+
+    def get_queryset(self):
+        return super().get_queryset().filter(usuario=self.request.user)
+
+
+class FinancasCartaoCreditoListView(LoginRequiredMixin, ListView):
+    model = models.CartaoCredito
+    template_name = "cartao_credito_list.html"
+    context_object_name = "cartao_credito"
+
+    def get_queryset(self):
+        return super().get_queryset().filter(usuario=self.request.user)
+
+
+class FinancasCartaoCreditoCreateView(LoginRequiredMixin, CreateView):
+    model = models.CartaoCredito
+    template_name = "cartao_credito_create.html"
+    form_class = forms.CartaoCreditoForm
+    success_url = reverse_lazy("financas:listarcartao")
+
+    def form_valid(self, form):
+        form.instance.usuario = self.request.user
+        return super().form_valid(form)
+
+
+class FinancasCartaoCreditoUpdateView(LoginRequiredMixin, UpdateView):
+    model = models.CartaoCredito
+    template_name = "cartao_credito_update.html"
+    form_class = forms.CartaoCreditoForm
+    success_url = reverse_lazy("financas:listarcartao")
+
+    def get_queryset(self):
+        return super().get_queryset().filter(usuario=self.request.user)
+
+    def form_valid(self, form):
+        form.instance.usuario = self.request.user
+        return super().form_valid(form)
+
+
+class FinancasCartaoCreditoDetailView(LoginRequiredMixin, DetailView):
+    model = models.CartaoCredito
+    template_name = "cartao_credito_detail.html"
+    context_object_name = "cartao_credito"
+
+    def get_queryset(self):
+        return super().get_queryset().filter(usuario=self.request.user)
